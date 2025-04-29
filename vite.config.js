80dee8aea626949abf17a2fa41009de0e0f8b5e1
althaf <althaf@gmail.com>
2025-04-29 13:01:39 +0530

    s

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/xpalicoportfolio/',  // <- add this line
+  base: '/animexpalico/',  // 👈 this must match the GitHub repo name exactly
   plugins: [react()],
 })
-
-
